c310d7a9672a3b497356c230ece202ce2ee7a168
Jacob Senecal <jsen@Jacobs-MacBook-Pro.local>
2018-02-16 13:00:14 -0700

    spectral information divergence function

--- utils.py
+++ utils.py
@@ -25,6 +25,28 @@ def spectral_correlation(cube1, cube2):
 
     return numerator/denominator
 
+
+def spectral_info_divergence(cube1, cube2):
+    cube1.select_region('Select Region to Compare', cube1.collect_spectra)
+    cube2.select_region('Select Region to Compare', cube2.collect_spectra)
+
+    cube1_spectra = np.clip(np.array(cube1.spectra), 0.01, None)
+    cube2_spectra = np.clip(np.array(cube2.spectra), 0.01, None)
+
+    cube1_probability = [wavelength/np.sum(cube1_spectra) for wavelength in cube1_spectra]
+    cube2_probability = [wavelength/np.sum(cube2_spectra) for wavelength in cube2_spectra]
+
+    n = len(cube1_probability)
+
+    entropy_xy = np.sum([cube1_probability[i] * np.log2(cube2_probability[i]/cube1_probability[i])
+                        for i in range(n)])
+
+    entropy_yx = np.sum([cube2_probability[i] * np.log2(cube1_probability[i]/cube2_probability[i])
+                        for i in range(n)])
+
+    return entropy_xy + entropy_yx
+
+
 def create_spectra_list(image_names):
     """
     Given a list of image names/locations, return a
