ad6415596f5e66efa7f0bcc6d3a31ab0c2fabec3
neil <neilswalton@gmail.com>
2018-04-04 21:17:25 -0600

    multiclass mlp working, needs cleanup

--- classification.py
+++ classification.py
@@ -1,6 +1,6 @@
 import pickle
 import numpy as np
-import tensorflow
+import tensorflow as tf
 from sklearn.svm import SVC
 from random import shuffle
 from sklearn.preprocessing import normalize
@@ -142,13 +142,87 @@ class classifier:
             prediction = self.clf.predict(test_input.reshape(1,-1))
             print prediction, self.test_labels[i]
 
+    def _labels_to_floats(self):
+        """
+        Convert string labels to floats
+        """
+        for i in range(len(self.train_labels)):
+            if str(self.train_labels[i]) == "banana":
+                self.train_labels[i] = np.array([1.0, 0.0, 0.0])
+            elif str(self.train_labels[i]) == "tomato":
+                self.train_labels[i] = np.array([0.0, 1.0, 0.0])
+            elif str(self.train_labels[i]) == "potato":
+                self.train_labels[i] = np.array([0.0, 0.0, 1.0])
+            else:
+                print "bad data"
+
+
     def _test_mlp(self):
         """
         Test the MLP's performance on the data
         """
 
-        pass
+        self._labels_to_floats()
 
+        learning_rate = 0.001
+        momentum = 0.3
+        iterations = 200
+        batch_size = 100
+        display_step = 1
+        input_size = len(self.inputs[0])
+        output_size = len(self.valid_labels)
+        hidden_size_1 = 15
+        hidden_size_2 = 15
+
+        #network weights
+        w1 = tf.Variable(tf.random_normal([input_size,hidden_size_1]))
+        w2 = tf.Variable(tf.random_normal([hidden_size_1,hidden_size_2]))
+        w_out = tf.Variable(tf.random_normal([hidden_size_2,output_size]))
+
+        #layer biases
+        b1 = tf.Variable(tf.random_normal([hidden_size_1]))
+        b2 = tf.Variable(tf.random_normal([hidden_size_2]))
+        b_out = tf.Variable(tf.random_normal([1]))
+
+        #input and output placeholders
+        x = tf.placeholder("float", [None, input_size])
+        y = tf.placeholder("float", [None, output_size])
+
+        #network layers
+        layer1 = tf.nn.relu(tf.add(tf.matmul(x, w1), b1))
+        layer2 = tf.nn.relu(tf.add(tf.matmul(layer1, w2), b2))
+        layer_out = tf.add(tf.matmul(layer2, w_out), b_out)
+
+        #RMSE loss and momentum optimizer
+        #loss = tf.sqrt(tf.reduce_mean(tf.square(layer_out-y)))
+        #optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, 
+        #                momentum=momentum, use_nesterov=True).minimize(loss)
+        loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=layer_out, labels=y))
+
+        optimizer = tf.train.AdamOptimizer(0.1).minimize(loss)
+
+        init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+
+            # Run the initializer
+            sess.run(init)
+
+            for i in range(iterations):
+
+                _, l = sess.run([optimizer, loss], feed_dict=
+                    {x: self.train_in, y:self.train_labels})
+                print(l)
+
+            print("-----------------------------")
+
+
+            out = sess.run(layer_out, feed_dict={x: self.test_in})
+            for i in range(len(self.test_in)):
+                #print ("in: " + str(data_in[i]) + "      out: " + str(out[i]) + "   true: " + str(data_out[i]))
+                print ("out: " + str(out[i]) + 
+                    "   true: " + str(self.test_labels[i]))
 
 if __name__ == '__main__':
 
@@ -159,5 +233,5 @@ if __name__ == '__main__':
         "No file found..."
 
     classify = classifier(data, norm=True)
-    classify.set_classifier('knn')
+    classify.set_classifier('mlp')
     classify.test()
