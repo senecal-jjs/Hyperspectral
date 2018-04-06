e0455f2666e08ecda862693ccff9f79a588e7ff2
neil <neilswalton@gmail.com>
2018-04-05 23:40:32 -0600

    Classifying chunks, need to recombine into image

--- classification.py
+++ classification.py
@@ -5,6 +5,8 @@ from sklearn.svm import SVC
 from random import shuffle
 from sklearn.preprocessing import normalize
 from sklearn.neighbors import KNeighborsClassifier
+import utils
+import image
 
 
 class classifier:
@@ -18,6 +20,7 @@ class classifier:
         self.train_in, self.train_labels, self.test_in, self.test_labels = self._split_data()
         self.clf = None
         self.classifier_type = None
+        self.correct = 0
 
     def _extract_input(self, norm):
         """
@@ -152,7 +155,20 @@ class classifier:
             index = self.label_encode[self.train_labels[i]]
             encoded_label[index] = 1.0
 
-            self.train_labels[i] = encoded_label        
+            self.train_labels[i] = encoded_label
+
+    def _all_labels_to_floats(self):
+        """
+        Convert string labels to floats
+        """
+
+        for i in range(len(self.labels)):
+
+            encoded_label = np.zeros(len(self.valid_labels))
+            index = self.label_encode[self.labels[i]]
+            encoded_label[index] = 1.0
+
+            self.labels[i] = encoded_label
 
     def _create_encoder(self):
         """
@@ -241,8 +257,8 @@ class classifier:
                 print ("out: " + self.label_decode[np.argmax(out[i])] + 
                     "   true: " + str(self.test_labels[i]))
 
-            if self.label_decode[np.argmax(out[i])] == str(self.test_labels[i]):
-                self.correct += 1 
+                if self.label_decode[np.argmax(out[i])] == str(self.test_labels[i]):
+                    self.correct += 1 
 
     def leave_one_out(self):
         """
@@ -272,18 +288,99 @@ class classifier:
             self.test_in = self.inputs[-1:]
             self.test_labels = self.labels[-1:]
 
-
-
         self.train_in = self.inputs[:i] + self.inputs[i+1:]
         self.train_labels = self.labels[:i] + self.labels[i+1:]
         self.test_in = self.inputs[i:i+1]
         self.test_labels = self.labels[i:i+1]
 
+    def classify_new_image(self):
+        """
+        Train the network, then classify each
+        square of a divided image
+        """
+
+        n=10
+        file_path = 'YukonGold_Tomato_Banana_1_Day7.bil'
+        raw_image = image.HyperCube(file_path)
+        original_shape = raw_image.image.shape
+        orig_x = original_shape[0]
+        orig_y = original_shape[1]
+        divided_image_reflectances = utils.avg_spectra_divided_image(raw_image, n)
+
+        self._create_encoder()
+        self._all_labels_to_floats()
+
+        learning_rate = 0.01
+        momentum = 0.3
+        iterations = 2000
+        batch_size = 100
+        display_step = 50
+        input_size = len(self.inputs[0])
+        output_size = len(self.valid_labels)
+        hidden_size_1 = 15
+        hidden_size_2 = 15
+
+        #network weights
+        w1 = tf.Variable(tf.random_normal([input_size,hidden_size_1]))
+        w2 = tf.Variable(tf.random_normal([hidden_size_1,hidden_size_2]))
+        w_out = tf.Variable(tf.random_normal([hidden_size_2,output_size]))
+
+        #layer biases
+        b1 = tf.Variable(tf.random_normal([hidden_size_1]))
+        b2 = tf.Variable(tf.random_normal([hidden_size_2]))
+        b_out = tf.Variable(tf.random_normal([1]))
+
+        #input and output placeholders
+        x = tf.placeholder("float", [None, input_size])
+        y = tf.placeholder("float", [None, output_size])
+
+        #network layers
+        layer1 = tf.nn.relu(tf.add(tf.matmul(x, w1), b1))
+        layer2 = tf.nn.relu(tf.add(tf.matmul(layer1, w2), b2))
+        layer_out = tf.add(tf.matmul(layer2, w_out), b_out)
+
+        #RMSE loss and momentum optimizer
+        #loss = tf.sqrt(tf.reduce_mean(tf.square(layer_out-y)))
+        #optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, 
+        #                momentum=momentum, use_nesterov=True).minimize(loss)
+        loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=layer_out, labels=y))
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+        classified_image = []
+        init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+
+            # Run the initializer
+            sess.run(init)
+
+            for i in range(iterations):
+
+                _, l = sess.run([optimizer, loss], feed_dict=
+                    {x: self.inputs, y:self.labels})
+                
+                if i% display_step == 0.5:
+                    print("loss after iteration " + str(i) + " is: " + str(l))
+
+            print("-----------------------------")
+
+
+            out = sess.run(layer_out, feed_dict={x: divided_image_reflectances})
+            for i in range(len(divided_image_reflectances)):
+                #print ("in: " + str(data_in[i]) + "      out: " + str(out[i]) + "   true: " + str(data_out[i]))
+                #print ("out: " + self.label_decode[np.argmax(out[i])])
+                classified_image.append(self.label_decode[np.argmax(out[i])])
+
+            classified_image = np.array(classified_image)
+            classified_image = np.reshape(classified_image, (orig_x/n, orig_y/n, 1))
+            return classified_image
 
 if __name__ == '__main__':
 
     try:
-        data = pickle.load( open( "avg_whole_refl.p", "rb" ))
+        data = pickle.load( open( "avg_class_refl.p", "rb" ))
 
     except (OSError, IOError) as e:
         "No file found..."
@@ -291,4 +388,9 @@ if __name__ == '__main__':
     classify = classifier(data, norm=True)
     classify.set_classifier('mlp')
     #classify.test()
-    classify.leave_one_out()
+    #classify.leave_one_out()
+
+    #print float(classify.correct)/len(classify.test_in)
+
+    image_classes = classify.classify_new_image()
+    print image_classes
